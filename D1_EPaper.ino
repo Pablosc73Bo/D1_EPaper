e68132276e204e7a66e86b138c327165982b350a
Paolo Colombari <paolo.colombari.bologna@gmail.com>
2025-11-14 11:30:45 +0100

    Update D1_EPaper.ino
    Ottimizzazione Display

--- D1_EPaper.ino
+++ D1_EPaper.ino
@@ -24,17 +24,11 @@ void setup() {
   display.setRotation(0);
   display.setTextColor(GxEPD_BLACK);
   display.setFullWindow();
-//  display.setPartialWindow (1, 1, 199, 199);
-
-//  display.writeScreenBuffer();  
-
-//  display.refresh(true);
   display.refresh(false);
-
   display.fillScreen(GxEPD_WHITE);
-
   display.firstPage();                                  // Inizializza e prepara il display
 
+/* 
 //  display.setFont(&FreeSerif18pt7b);
   display.setCursor(10, 30);
   display.setTextSize(3);
@@ -49,6 +43,25 @@ void setup() {
   display.print("D1_EPaper");
 
   display.nextPage();
+*/
+
+do {
+  //display.setFont(&FreeSerif18pt7b);
+  display.setCursor(10, 30);
+  display.setTextSize(3);
+  display.print("Colombari");
+
+  display.setCursor(10, 70);
+  display.setTextSize(3);
+  display.print("Paolo Boss");
+
+  display.setCursor(10, 110);
+  display.setTextSize(2);
+  display.print("D1_EPaper");
+} while (display.nextPage()); // Gestisce buffer + display automaticamente
+
+
+
   display.powerOff();
   delay(5000); 
 
@@ -61,6 +74,7 @@ void loop() {
 
     Valore++;
  
+/*
     // Tutto il codice di disegno va qui dentro
     display.setPartialWindow (50, 2, 50, 16);
     display.fillScreen(GxEPD_WHITE);
@@ -68,9 +82,22 @@ void loop() {
     display.setCursor(50, 2);
     display.print(Valore);
     display.nextPage();
-    display.powerOff();
-    delay(600); 
+*/
 
+do {
+    display.setPartialWindow (50, 2, 50, 16);
+    display.fillScreen(GxEPD_WHITE);
+    display.setTextSize(2);
+    display.setCursor(50, 2);
+    display.print(Valore);
+} while (display.nextPage()); // Gestisce buffer + display automaticamente
+
+
+    //display.powerOff();
+    display.hibernate();
+    delay(1000); 
+
+/*
     // Tutto il codice di disegno va qui dentro
     display.setPartialWindow (100, 150, 50, 16);
     display.fillScreen(GxEPD_WHITE);
@@ -78,32 +105,26 @@ void loop() {
     display.setCursor(100, 150);
     display.print(Valore*3);
     display.nextPage();
+*/
+
+do {
+    display.setPartialWindow (100, 150, 50, 16);
+    display.fillScreen(GxEPD_WHITE);
+    display.setTextSize(2);
+    display.setCursor(100, 150);
+    display.print(Valore*3);
+} while (display.nextPage()); // Gestisce buffer + display automaticamente
+
+
 
  
-    display.powerOff();
-    delay(600); 
+    //display.powerOff();
+    display.hibernate();
+    delay(1000); 
   // put your main code here, to run repeatedly:
 
 
-
-/*
-    display.setPartialWindow (1, 20, 48, 16);
-    display.fillScreen(GxEPD_WHITE);
-    display.setCursor(1, 20);
-    display.print(Valore);
-    display.nextPage();
-*/
 //  display.hibernate();
-//  display.powerOff();
 
-/*
-    display.setPartialWindow (100, 20, 48, 16);
-    display.fillScreen(GxEPD_WHITE);
-    display.setCursor(100, 20);
-    display.print(Valore);
-    display.nextPage();
-*/
-
- // delay(1000);  
 
 }
